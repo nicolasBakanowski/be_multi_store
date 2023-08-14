26f467f2e2210a70534a772708508e66928a90f2
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-08-14 18:18:09 -0300

    chore: add tables and seeder to usear and rol

---
+++ src/migrations/20230814062510-create-users-table.js
@@ -0,0 +1,53 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nombre: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      googleId: {
+        type: Sequelize.STRING, // Campo para el inicio de sesión con Google
+      },
+      telefono: {
+        type: Sequelize.STRING, // Campo para el número de teléfono
+      },
+      roleId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Roles",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
+};
