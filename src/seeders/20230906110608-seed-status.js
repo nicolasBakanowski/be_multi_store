0379b3f810892bde21e6c7e58bdc7f7383c177d4
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-11-28 01:35:06 -0300

    fix: seed Status file

--- src/seeders/20230906110608-seed-status.js
+++ src/seeders/20230906110608-seed-status.js
@@ -4,17 +4,26 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Status", [
       {
+        id: 1,
         name: "pendiente",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 2,
         name: "confirmado",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 3,
         name: "terminado",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 4,
+        name: "rechazado",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
