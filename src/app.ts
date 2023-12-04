b5a02b7f24bd7dc07aca71757648ae0fef2339b1
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-04 18:32:10 -0300

    fix problem with images

--- src/app.ts
+++ src/app.ts
@@ -38,6 +38,16 @@ const io = new Server(server, {
     credentials: true,
   },
 });
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://multi-store-git-develop-nicolasbakanowski.vercel.app"
+  );
+  // Otros encabezados CORS si es necesario
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // Config express-rate-limit
 const limiter = rateLimit({
