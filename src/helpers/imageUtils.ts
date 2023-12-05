ac9f6af888a56f3fec3fc0303e1fe29b258cedb7
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-05 19:58:11 -0300

    check route

--- src/helpers/imageUtils.ts
+++ src/helpers/imageUtils.ts
@@ -1,7 +1,7 @@
 import multer from "multer";
 import path from "path";
 import { Request } from "express";
-import fs from "fs";
+const fs = require("fs");
 
 function imageProcessFunction(
   req: Request,
@@ -20,7 +20,14 @@ function imageDestination(
   console.log(itemType, "itemType");
   console.log(_file, "tiene un archivo esto? ");
   const destination = path.join("uploads", itemType);
-  fs.mkdirSync(destination, { recursive: true });
+  console.log("destination", destination);
+  fs.access(destination, fs.constants.F_OK, (err: any) => {
+    if (err) {
+      console.error("La ruta no existe", err);
+    } else {
+      console.log("La ruta existe");
+    }
+  });
   cb(null, destination);
 }
 
