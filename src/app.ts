75a3ff75b3d65b350f4ab838e709d93be456f59d
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-11 03:30:09 -0300

    try fix route

--- src/app.ts
+++ src/app.ts
@@ -66,7 +66,7 @@ app.use(
 );
 app.use(
   "/dist/uploads/category/",
-  express.static(path.join(__dirname, "dist", "uploads", "category"))
+  express.static(path.join(__dirname, "uploads", "category"))
 );
 
 app.get("/test", (req, res) => {
