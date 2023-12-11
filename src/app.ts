3b099c0eaa797682f2025f5c37c190c73feb1e91
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-11 03:06:16 -0300

    try fix images

--- src/app.ts
+++ src/app.ts
@@ -62,11 +62,11 @@ app.use(limiter);
 
 app.use(
   "/dist/uploads/product/",
-  express.static(path.join(__dirname, "..", "uploads", "product"))
+  express.static(path.join(__dirname, "dist", "uploads", "product"))
 );
 app.use(
   "/dist/uploads/category/",
-  express.static(path.join(__dirname, "..", "uploads", "category"))
+  express.static(path.join(__dirname, "dist", "uploads", "category"))
 );
 
 app.get("/test", (req, res) => {
