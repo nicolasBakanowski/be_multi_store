e49c76f99a021e86b925fd5b98a10bfbf1c88dce
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-03-06 02:58:49 -0300

    fix seeds

--- src/seeders/20230814062705-roles-seed.js
+++ src/seeders/20230814062705-roles-seed.js
@@ -2,26 +2,28 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("Roles", [
-      {
-        name: "Admin",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "Usuario",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "Empledo",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    // Eliminar datos existentes
+    await queryInterface.bulkDelete("Roles", null, {});
+
+    // Insertar nuevos datos con IDs específicos
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.bulkInsert(
+        "Roles",
+        [
+          { id: 1, name: "Admin", createdAt: new Date(), updatedAt: new Date() },
+          { id: 2, name: "Usuario", createdAt: new Date(), updatedAt: new Date() },
+          { id: 3, name: "Empleado", createdAt: new Date(), updatedAt: new Date() },
+        ],
+        { transaction }
+      );
+
+      // Reiniciar la secuencia de autoincrementación a 1
+      await queryInterface.sequelize.query("ALTER TABLE Roles AUTO_INCREMENT = 1", { transaction });
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
+    // Eliminar datos existentes
     await queryInterface.bulkDelete("Roles", null, {});
   },
 };
