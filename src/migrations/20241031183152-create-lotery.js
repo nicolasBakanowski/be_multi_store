77725512935b4e8701265dbf0b78b7732e2b8d8b
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-10-31 15:38:15 -0300

    add migrations to lotery flow

---
+++ src/migrations/20241031183152-create-lotery.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Lotteries", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      requiredEarnings: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "active",
+      },
+      winnerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users", 
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Lotteries");
+  },
+};
