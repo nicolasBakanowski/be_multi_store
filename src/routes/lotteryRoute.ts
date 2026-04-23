8c679d259908cfbc6fb991ad4bae5e5f5d53cffc
Nicolas <nicolasbakanowski@gmail.com>
2026-04-23 18:12:01 -0300

    feat: add authentication and admin checks to start new lottery route

--- src/routes/lotteryRoute.ts
+++ src/routes/lotteryRoute.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { startNewLotteryController, getCurrentLotteryController } from "../controllers/lotteryController";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { isAdminMiddleware } from "../middleware/isAdminMiddleware";
 
 const lotteryRoute = express.Router();
 
-lotteryRoute.post("/start", startNewLotteryController);
+lotteryRoute.post("/start", authMiddleware, isAdminMiddleware, startNewLotteryController);
 
 lotteryRoute.get("/current", getCurrentLotteryController);
 
